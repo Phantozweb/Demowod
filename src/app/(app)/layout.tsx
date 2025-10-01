be5c2418830187d8c8092cfab008771a444dc2e7
janarthan <iamsirenjeev@gmail.com>
2025-10-01 12:10:50 +0000

    Remove entire side bar (_for element <SidebarContent>_)

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,15 +1,5 @@
-import AppSidebar from "@/components/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { SidebarInset } from "@/components/ui/sidebar";
-
-
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <main className="min-h-svh w-full bg-background text-foreground">{children}</main>
-      </SidebarInset>
-    </SidebarProvider>
+    <main className="min-h-svh w-full bg-background text-foreground">{children}</main>
   );
 }
