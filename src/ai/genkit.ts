ed82c2975d3565f87e76f6686ebe771d75aae304
janarthan <iamsirenjeev@gmail.com>
2025-10-08 18:24:47 +0000

    Within code add api key

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/google-genai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
+      apiKey: 'AIzaSyD5WkVoZK-Hrm7MTOUa-xkpaHwfVXW2OJI',
     }),
   ],
   model: 'googleai/gemini-2.5-flash',
