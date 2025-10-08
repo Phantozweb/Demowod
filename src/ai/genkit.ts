6f7c88f96703fe90b1ab54d3a3150ccd5d06f953
janarthan <iamsirenjeev@gmail.com>
2025-10-08 20:10:01 +0000

    Set enviornment to work on enn not within code

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,13 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import { config } from 'dotenv';
+
+config();
 
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: 'AIzaSyD5WkVoZK-Hrm7MTOUa-xkpaHwfVXW2OJI',
+      apiKey: process.env.GEMINI_API_KEY,
     }),
   ],
   model: 'googleai/gemini-2.5-flash',
